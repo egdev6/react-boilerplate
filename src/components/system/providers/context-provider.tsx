6d3384f53646ae0b49e2565afb4f768a5731e550
egdev <egdev6@gmail.com>
2025-04-01 01:49:05 +0200

    Resolved merge conflicts and updated files

--- src/components/system/providers/context-provider.tsx
+++
@@ -1,7 +0,0 @@
-import { AppContextProvider } from '@hooks/use-app-context';
-
-const CustomAppContextProvider = ({ children }: React.PropsWithChildren) => (
-  <AppContextProvider>{children}</AppContextProvider>
-);
-
-export default CustomAppContextProvider;
--- src/models/context.ts
+++ src/models/context.ts
@@ -9,3 +9,26 @@ export type CharactersContextValue = {
   characters: Character[] | undefined;
   setCharacters: (characters: Character[] | undefined) => void;
 };
+
+export type DeviceInfoContextValue = {
+  platform?: 'Web' | 'Android native' | 'IOs native' | 'WebView Android' | 'WebView IOs';
+  browser?: 'Safari' | 'Edge' | 'Chrome' | 'Firefox';
+  system?: 'IOs' | 'Android' | 'Windows' | 'Mac' | 'Linux';
+  screenSize: { width: number; height: number };
+  isMobileByAgent: boolean;
+  isTabletByAgent: boolean;
+  isDesktopByAgent: boolean;
+  isMobileByScreen: () => boolean;
+  isTabletByScreen: () => boolean;
+  isDesktopByScreen: () => boolean;
+  isNative: boolean;
+  isSafari: boolean;
+  isEdge: boolean;
+  isChrome: boolean;
+  isFirefox: boolean;
+  isIOs: boolean;
+  isAndroid: boolean;
+  isWindows: boolean;
+  isMacOS: boolean;
+  isLinux: boolean;
+};
