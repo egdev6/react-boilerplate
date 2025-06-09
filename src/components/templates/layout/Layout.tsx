f8425acb36f15c77f4f9b86c0be7a15b5bdd3a0b
Quique <enrique.gomez@scrm.lidl>
2025-06-09 09:43:05 +0200

    [UPDATE] folders modified + home page added + storybook removed

--- src/components/templates/layout/Layout.tsx
+++ src/components/templates/layout/Layout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout: FC = (): JSX.Element | null => {
   return (
-    <div className='w-full min-h-dvh bg-[image:var(--gradient-background)] dark:bg-[image:var(--gradient-dark-background)]'>
+    <div className='w-full min-h-dvh bg-background-light dark:bg-background-dark'>
       <Header />
       <div className='w-full h-full pt-20 min-h-[calc(100dvh-130px)] relative bg-transparent'>
         <div className='w-full h-auto min-h-[calc(100dvh-130px)] max-w-[1248px] px-6 sm:px-6 py-6 sm:py-12 mx-auto flex flex-col items-center justify-center'>
